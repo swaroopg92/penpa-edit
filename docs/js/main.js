ea1e96678d8fdd747d0bb2ea1ffea713f1e36b36
opt-pan <52611232+opt-pan@users.noreply.github.com>
2019-07-16 10:50:37 +0900

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -250,6 +250,11 @@ function modaldelete(e) {
     modal3.style.display = 'none';
   }
 }
+  
+var buttons = document.getElementById("buttons");
+buttons.addEventListener("ontouchstart", function(e){
+    e.preventDefault();
+},{passive: false});
 
 //drag_window
 //要素の取得
