f36077c1724346d0597f6df6f33175441ec98721
Swaroop <guggi022@umn.edu>
2020-06-18 23:27:14 -0400

    translated correct answer

--- docs/js/class_p.js
+++ docs/js/class_p.js
@@ -3279,7 +3279,7 @@ class Puzzle{
       var text = JSON.stringify(this.make_solution());
       if(text === this.solution &&this.sol_flag === 0){
         setTimeout(() => {
-          alert("正解です")
+          alert("Correct Answer")
         }, 10)
         this.sol_flag = 1;
       }else if(text != this.solution &&this.sol_flag === 1){//答えが変わったら改めて判定
