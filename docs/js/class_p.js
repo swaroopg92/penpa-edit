c7def2a70e65af098d714056bb87b1258685e0e1
Swaroop <guggi022@umn.edu>
2020-08-04 00:28:06 -0400

    added more colors to number styles and improved display of the buttons

--- docs/js/class_p.js
+++ docs/js/class_p.js
@@ -819,7 +819,7 @@ class Puzzle {
 
         for (var i in this[pu].number) {
             // Sudoku only one number and multiple digits in same cell should not be considered
-            // if ((this[pu].number[i][1] === 2 || this[pu].number[i][1] === 8) && this[pu].number[i][2] === "7") {
+            // if ((this[pu].number[i][1] === 2 || this[pu].number[i][1] === 8 || this[pu].number[i][1] === 9) && this[pu].number[i][2] === "7") {
             //     var sum = 0,
             //         a;
             //     for (var j = 0; j < 10; j++) {
@@ -833,7 +833,7 @@ class Puzzle {
             //     }
             // } else 
             if (!isNaN(this[pu].number[i][0]) || !this[pu].number[i][0].match(/[^A-Za-z]+/)) {
-                if ((this[pu].number[i][1] === 2 || this[pu].number[i][1] === 8) && (this[pu].number[i][2] === "1" || this[pu].number[i][2] === "5" || this[pu].number[i][2] === "6" || this[pu].number[i][2] === "10")) {
+                if ((this[pu].number[i][1] === 2 || this[pu].number[i][1] === 8 || this[pu].number[i][1] === 9) && (this[pu].number[i][2] === "1" || this[pu].number[i][2] === "5" || this[pu].number[i][2] === "6" || this[pu].number[i][2] === "10")) {
                     sol[4].push(i + "," + this[pu].number[i][0]);
                 }
             }
