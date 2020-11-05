5a08fd0098f143aa57bc511a477e78bc9d35beb8
Swaroop <guggi022@umn.edu>
2020-11-04 22:25:18 -0500

    improved alert message of correct answer, layed field for erase combi mode

--- docs/js/class_p.js
+++ docs/js/class_p.js
@@ -733,6 +733,42 @@ class Puzzle {
             case "special":
                 this[this.mode.qa][this.mode[this.mode.qa][this.mode[this.mode.qa].edit_mode][0]] = [];
                 break;
+            case "combi":
+                switch (this.mode[this.mode.qa][this.mode[this.mode.qa].edit_mode][0]) {
+                    case "tents":
+                        break;
+                    case "linex":
+                        break;
+                    case "edgexoi":
+                        break;
+                    case "blpo":
+                        break;
+                    case "blwh":
+                        break;
+                    case "battleship":
+                        break;
+                    case "star":
+                        break;
+                    case "magnets":
+                        break;
+                    case "lineox":
+                        break;
+                    case "yajilin":
+                        break;
+                    case "hashi":
+                        break;
+                    case "arrowS":
+                        break;
+                    case "shaka":
+                        break;
+                    case "numfl":
+                        break;
+                    case "alfl":
+                        break;
+                    case "edgesub":
+                        break;
+                }
+                break;
         }
         this.redraw();
     }
@@ -6056,8 +6092,14 @@ class Puzzle {
             var text = JSON.stringify(this.make_solution());
             if (text === this.solution && this.sol_flag === 0) {
                 setTimeout(() => {
-                    alert("Correct Answer")
-                }, 10)
+                    Swal.fire({
+                        title: 'Swaroop says:',
+                        html: '<h2 style="color:blue;">Congratulations!!! Well done!!!</h2>',
+                        icon: 'success',
+                        confirmButtonText: 'Hurray!',
+                        // timer: 5000
+                    })
+                }, 20)
                 sw_timer.stop();
                 this.mouse_mode = "out";
                 this.mouseevent(0, 0, 0);
