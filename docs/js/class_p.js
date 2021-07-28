9bbc94257acffdb839d152ad4a0208f889e38121
Swaroop <guggi022@umn.edu>
2021-07-27 21:04:53 -0400

    added version tag to javascript loading to avoid cache problems

--- docs/js/class_p.js
+++ docs/js/class_p.js
@@ -157,7 +157,7 @@ class Puzzle {
             ["\"__a\"", "z_"],
             ["null", "zO"],
         ];
-        this.version = [2, 26, 2];
+        this.version = [2, 26, 2]; // Also defined in HTML Script Loading in header tag to avoid Browser Cache Problems
         this.undoredo_disable = false;
         this.comp = false;
         this.multisolution = false;
