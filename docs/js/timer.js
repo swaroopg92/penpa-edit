c251bfba5f20034700bd526724fe2714fbe4af1b
Swaroop <guggi022@umn.edu>
2021-08-25 16:19:57 -0400

    timer hide button, updated dark_theme css for pausecanvas

--- docs/js/timer.js
+++ docs/js/timer.js
@@ -8,6 +8,18 @@ $('#stop_watch #sw_reset').click(function() {
     sw_timer.reset();
 });
 
+$('#stop_watch #sw_hide').click(function() {
+    let timer_value = document.getElementById("timer");
+    let hide_button = document.getElementById("sw_hide");
+    if (hide_button.textContent === "Hide") {
+        timer_value.style.display = "None";
+        hide_button.textContent = "UnHide";
+    } else {
+        timer_value.style.display = "inline-block";
+        hide_button.textContent = "Hide";
+    }
+});
+
 sw_timer.addEventListener('secondsUpdated', function(e) {
     $('#stop_watch .values').html(sw_timer.getTimeValues().toString(['days', 'hours', 'minutes', 'seconds', 'secondTenths']));
 });
