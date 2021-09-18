84538afa54a1235e5b763320eb864f34357212e6
Swaroop <guggi022@umn.edu>
2021-09-18 11:29:29 -0400

    added genres list based on gmpuzzles categories

---
+++ docs/js/genre_tags.js
@@ -0,0 +1,65 @@
+const penpa_tags = {
+    "options_groups": ["Sudoku", "Number Placement", "Object Placement", "Shading", "Region Division", "Loop/Path", "Others"],
+    "options": {
+        "Sudoku": [
+            "classic",
+            "arrow",
+            "thermo",
+            "tightfit",
+            "evenodd",
+            "consecutivepairs",
+            "killer",
+            "isodoku",
+            "consecutive",
+            "sudokurve",
+            "tile",
+            "battleship",
+            "deficit",
+            "surplus",
+            "outside"
+        ],
+        "Number Placement": [
+            "tomtom",
+            "skyscrapers",
+            "kakuro"
+        ],
+        "Object Placement": [
+            "starbattle",
+            "battleships",
+            "statuepark",
+            "minesweeper"
+        ],
+        "Shading": [
+            "tapa",
+            "lits",
+            "nurikabe",
+            "crossthestreams",
+            "nanro",
+            "kurotto",
+        ],
+        "Region Division": [
+            "fillomino",
+            "cave",
+            "pentominous",
+            "araf",
+            "spiralgalaxies",
+            "snakepit",
+            "kuromasu"
+        ],
+        "Loop/Path": [
+            "masyu",
+            "slitherlink",
+            "yajilin",
+            "castlewall",
+            "balanceloop",
+            "snake",
+            "tapalikeloop",
+            "roundtrip"
+        ],
+        "Others": [
+            "wordpuzzle",
+            "puzzlehunt",
+            "wordsearch"
+        ]
+    }
+}
