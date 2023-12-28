636894649da8cda9da8b6fffca704fd9ed116e4d
Mark <mark.langezaal@gmail.com>
2023-12-29 00:51:06 +0100

    Change 'Tapa' to 'Quad' on Sudoku UI
    Tapa is 'problem mode' only, so there is no need to gate this with a version check.

--- docs/js/interface.js
+++ docs/js/interface.js
@@ -78,5 +78,6 @@ const PenpaUI = {
         for (var i of allPossible.exceptions) {
             document.getElementById(i).classList.toggle('is_hidden', selectedGrid.exceptions.indexOf(i) < 0);
         }
+        document.getElementById("sub_number4_lb").innerText = (gridtype === 'sudoku') ? 'Quad' : 'Tapa';
     }
 };
