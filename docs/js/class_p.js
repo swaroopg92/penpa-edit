5d369760e35d946d0b5ac8416feb0f8d4463584c
Swaroop Guggilam <penpaplus@gmail.com>
2025-05-08 17:52:11 -0400

    move redraw calls out of main loop of undo and redo to increase performance speed

--- docs/js/class_p.js
+++ docs/js/class_p.js
@@ -6535,9 +6535,9 @@ class Puzzle {
                             }
                         }
                     }
-                    this.redraw();
                 }
             }
+            this.redraw();
         } else {
             while (undocounter !== 0) {
                 var a = this.pu_a.command_undo.pop(); /*a[0]:list_name,a[1]:point_number,a[2]:value, a[4]: groupindex (optional)*/
@@ -6657,9 +6657,9 @@ class Puzzle {
                             this.pu_a_col.command_replay.push(a_col_replay);
                         }
                     }
-                    this.redraw();
                 }
             }
+            this.redraw();
         }
     }
 
@@ -6766,9 +6766,9 @@ class Puzzle {
                             }
                         }
                     }
-                    this.redraw();
                 }
             }
+            this.redraw();
         } else {
             while (redocounter !== 0) {
                 var a = this.pu_a.command_redo.pop();
@@ -6877,10 +6877,9 @@ class Puzzle {
                             this.pu_a_col.command_replay.push(a_col);
                         }
                     }
-
-                    this.redraw();
                 }
             }
+            this.redraw();
         }
     }
 
