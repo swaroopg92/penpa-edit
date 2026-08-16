c3a72811df120e7a392d950e9bc727b2a472bcf7
David Millar <dave@davegoesthedistance.com>
2026-08-16 17:12:38 -0500

    Add js-md5 to the bundled libs.

--- libs/main.js
+++ libs/main.js
@@ -2,8 +2,10 @@ import $ from 'jquery';
 import Swal from 'sweetalert2';
 import DOMPurify from 'dompurify';
 import localforage from 'localforage';
+import md5 from 'js-md5';
 
 window.jQuery = window.$ = $;
 window.Swal = Swal;
 window.DOMPurify = DOMPurify;
 window.localforage = localforage;
+window.md5 = md5;
